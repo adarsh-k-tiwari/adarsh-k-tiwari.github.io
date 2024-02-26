5093f792613f6dea2ed14cf78898432b84494ef9
Adarsh Kumar <1805695@kiit.ac.in>
2024-02-26 13:56:29 +0530

    Add files via upload

---
+++ js/script2.js
@@ -0,0 +1,13 @@
+
+
+$(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
